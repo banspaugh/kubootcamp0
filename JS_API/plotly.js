61240cfbf143fb36c9c2049e9ac15be60ff9ea4f
Blaine Anspaugh <spaugh56@yahoo.com>
2018-05-08 18:46:10 -0500

    web charting hw
    web charting hw

--- JS_API/plotly.js
+++ JS_API/plotly.js
@@ -1,6 +1,107 @@
 var someList = document.getElementById("selDataset");
 
-d3.json('/names', function(error, data){
-    console.log(data)
+    d3.json('/names', function (error, data) {
 
-});
+        for (var i = 0; i < data.length; i++) {
+            var nameItem = document.createElement("option");
+            var something = data[i];
+            nameItem.innerHTML = something;
+            someList.appendChild(nameItem);
+        }
+    });
+
+    function optionChanged(value) {
+        makeSumPie(value)
+        makeSumBubs(value)
+    }
+
+    function init() {
+        let current_thing = 'BB_940'
+        d3.json('/samples/' + current_thing, function (error, data) {
+            if (error) throw error;
+            var otu_id = []
+            var otu_value = []
+            var response = data
+            for (var i = 0; i < 10; i++) {
+                otu_id.push(data[0][i]);
+            }
+            for (var i = 0; i < 10; i++) {
+                otu_value.push(data[1][i]);
+            }
+
+            var data = [{
+                values: otu_value,
+                labels: otu_id,
+                type: "pie"
+            }];
+
+
+
+            Plotly.plot("pie", data);
+
+            var trace1 = {
+                x: response[0],
+                y: response[1],
+                mode: 'markers',
+                marker: {
+                    size: response[1],
+                    color: response[0]
+                }
+            };
+            var data1 = [trace1];
+
+            Plotly.newPlot('bubbles', data1);
+        });
+    }
+    init();
+
+    function makeSumPie(value) {
+        d3.json('/samples/' + value, function (error, data) {
+            if (error) throw error;
+            var otu_id = []
+            var otu_value = []
+            for (var i = 0; i < 10; i++) {
+                otu_id.push(data[0][i]);
+            }
+            for (var i = 0; i < 10; i++) {
+                otu_value.push(data[1][i]);
+            }
+
+            var data = [{
+                values: otu_value,
+                labels: otu_id,
+                type: "pie"
+            }];
+            updatePlotly(data);
+        });
+    }
+
+    function makeSumBubs(value) {
+        d3.json('/samples/' + value, function (error, data) {
+            if (error) throw error;
+
+            var trace1 = {
+                x: data[0],
+                y: data[1],
+                mode: 'markers',
+                marker: {
+                    size: data[1],
+                    color: data[0]
+                }
+            };
+            var data1 = [trace1];
+            console.log(data[0])
+            console.log(data[1])
+            updateBubs(data1)
+        });
+    }
+
+    function updatePlotly(newData) {
+        var PIE = document.getElementById("pie");
+        Plotly.newPlot(PIE, newData);
+    }
+
+    function updateBubs(newData) {
+        var bubs = document.getElementById("bubbles");
+        Plotly.newPlot(bubs, newData);
+    }
---
+++ Web Charting/logic.js
@@ -0,0 +1,116 @@
+
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var link2 = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  let markers = [];
+  console.log(data)
+  for (var i = 0; i < data.features.length; i++) {
+    // console.log(data.features[i].geometry.coordinates)
+    let x = data.features[i].geometry.coordinates[0]
+    let y = data.features[i].geometry.coordinates[1]
+    let size = data.features[i].properties.mag
+    let color = null;
+    let fill = null;
+    if (size < 1) {
+      color = '#1BC04C';
+      fill = '#1BC04C';
+    } else if (size <= 2) {
+      color ='FFFF33';
+      fill = '#FFFF33';
+     } else if (size <= 3) {
+       color ='#FFFF00';
+       fill = '#FFFF00';
+     } else if (size <= 4) {
+       color = '#FF7A04';
+       fill = '#FF7A04';
+     } else if (size <= 5) {
+       color = '#F35C1B';
+       fill = '#F35C1B';
+     } else if (size >= 5) {
+       color = '#FF0000';
+       fill = '#FF0000';
+     }
+
+    let marker = L.circleMarker([y, x], {radius : size*1.5, color : color, fillColor : fill, fillOpacity: 0.9})
+    markers.push(marker)
+  };
+  d3.json(link2, function(response) {
+//     map_coords = []
+
+//     for (var i = 0; i < response.features.length; i++) {
+//       coords = [];
+//       for (var j = 0; j < response.features[i].geometry.coordinates[0].length; j++) { 
+//         coords.push(response.features[i].geometry.coordinates[0][j] )
+//       }
+//       var geoDude= {'type' : response.features[i].geometry.type, 'coordinates': coords};
+//       // console.log(L.geoJSON(geoDude))
+//       map_coords.push(geoDude)
+
+//     }
+    
+//     var cityLayer = L.geoJSON(map_coords);
+//   // Overlays that may be toggled on or off
+
+// console.log(markers)
+// console.log(map_coords)
+
+    // Creating a GeoJSON layer with the retrieved data
+          // call create map function with marker list
+
+  // Creating map object
+
+
+// console.log(markers)
+// Adding tile layer
+
+var dataLayer = L.geoJson(response)
+var markerLayer = L.featureGroup(markers)
+var overlayMaps = {
+  "Tectonic Plates": dataLayer,
+  "Earthquakes": markerLayer
+};
+
+let mapboxUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+let accessToken = 'pk.eyJ1IjoicmluY2tkIiwiYSI6ImNpamc3ODR1aDAxMmx0c2x0Zm9lc3E1OTAifQ.pIkP7PdJMrR5TBIp93Dlbg';
+let myLayer = L.tileLayer(mapboxUrl, {id: 'mapbox.streets-satellite', maxZoom: 20, accessToken: accessToken});
+let light = L.tileLayer(mapboxUrl, {id: 'mapbox.streets', maxZoom: 20, accessToken: accessToken});
+let dark = L.tileLayer(mapboxUrl, {id: 'mapbox.dark', maxZoom: 20, accessToken: accessToken});
+
+var baseMaps = {
+  "Street": myLayer,
+  "Light": light,
+  "Dark": dark
+};
+
+var map = L.map("map", {
+  center: [39.5, -100],
+  zoom: 3,
+  layers: [myLayer, light, dark]
+});
+
+L.control.layers(baseMaps,overlayMaps).addTo(map);
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+    limits = [1,2,3,4,5,6],
+    labels = ['0-1','1-2','2-3','3-4','4-5', '5+']
+    var colors = ['green','#FFFF33','yellow','orange','#FF7A04','red'];
+    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
+    <div class="max">' + limits[limits.length - 1] + '</div></div>'
+  
+  limits.forEach(function (limit, index) {
+    labels.push('<li style="background-color: ' + colors[index] + '">' + labels[index] +'</li>')
+  })
+  
+  div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+    return div
+  }
+  legend.addTo(map)
+})
+  });
+
+
+
